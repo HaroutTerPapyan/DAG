5c96194669850afbe9d1051cd956aa71e47f46ed
Harout <haroutter@gmail.com>
2015-10-09 18:14:19 -0700

    Initial Commit

---
+++ src/DAG.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * Created by Harout on 10/9/2015.
+ */
+
+public class DAG {
+
+        private String inputFileName;
+        private int[][] adjMatrix;
+        private int[] visited;
+        private int[] paths;
+        //other private fields
+        private int numVertices;
+        private int numEdges;
+        private int[] parent;
+
+        public DAG(String inputFileName) throws Exception{
+            this.inputFileName = inputFileName;
+            numVertices = 0;
+            numEdges = 0;
+            readInputData();
+        }
+
+    private void readInputData() throws Exception{
+
+        java.io.File inputFile = new java.io.File(inputFileName);
+        Scanner input = new Scanner(inputFile);
+
+        numVertices = input.nextInt();
+        int a = 0, b = 0;
+        adjMatrix = new int[numVertices][numVertices];
+
+        while (input.hasNext()) {
+            a = input.nextInt();
+            b = input.nextInt();
+            numEdges++;
+            adjMatrix[a][b] = 1;
+        }
+    }
+
+}
